21c13586c73732b6273acd50a39f3efc05f5614e
Lucas da silva <lucasdasilva524@hotmail.com>
2020-11-21 18:20:36 -0300

    Desafio resolvido

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+
+  const [repository, setRepository] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepository(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Desafio ReactMobile ${Date.now()}`,
+      url: "https://github.com/lucasd-coder/desafio_nodejs_n1_conceitos.git",
+      techs: ["Node.js"]
+    });
+
+    const repositorys = response.data;
+
+    setRepository([...repository, repositorys]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    setRepository(repository.filter(
+      repositories => repositories.id !== id
+    ));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repository.map(repositories => (
+          <li key={repositories.id} >
+            {repositories.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+            <button onClick={() => handleRemoveRepository(repositories.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
